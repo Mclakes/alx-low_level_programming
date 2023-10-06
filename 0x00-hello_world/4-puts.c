a204107d6404da4fe01d964c96d4ed4fa41c09d1
mclakes <lekanhayo@gmail.com>
2023-10-05 17:29:57 -0700

    still number 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include <stdio.h>  
+#include <stdio.h>
 /**
 *main - main entry point
-*return: always 0(success)
+*return: always 0 (success)
 */
 int main(void)
-{	
+{
 	puts("\"Programming is like building a puzzle");
 	return (0);
 }
